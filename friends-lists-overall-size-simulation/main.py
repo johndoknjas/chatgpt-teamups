571bea3905f693733f95fc6eda0081765b71797b
johndoknjas <jdoknjas@sfu.ca>
2022-12-20 16:27:52 -0800

    Some cleanup of the code by me. I also fixed the issue where the runtime scaled quadratically(?) with the input size. Should be close enough to linear now.

--- friends-lists-overall-size-simulation/main.py
+++ friends-lists-overall-size-simulation/main.py
@@ -3,20 +3,14 @@ import sys
 import time
 
 def main():
-    # Total number of players on the server
     server_size = int(sys.argv[1])
-
-    # Number of players to generate friends lists for
     num_players = int(sys.argv[2])
-
-    # Lower and upper bounds for the number of friends
     lower_bound = int(sys.argv[3])
     upper_bound = int(sys.argv[4])
 
-    # List of sets containing the friends lists for the selected players
-    friends_lists = []
+    # List of friends in friends lists of selected players (will contain duplicates initially).
+    friends_in_f_lists = []
 
-    # Start time
     start_time = time.time()
 
     # Generate the friends lists for the selected players
@@ -25,24 +19,11 @@ def main():
         num_friends = random.randint(lower_bound, upper_bound)
         while len(friends) < num_friends:
             friends.add(random.randint(0, server_size - 1))
-        friends_lists.append(friends)
+        friends_in_f_lists.extend(list(friends))
 
-        # Print message after 10, 20, 30, etc. seconds of runtime
-        elapsed_time = time.time() - start_time
-        if elapsed_time >= 10 and int(elapsed_time) % 10 == 0:
-            print(f"{int(elapsed_time)} seconds elapsed")
-
-    # Print message after all iterations of the first for loop are completed
     print("First for loop completed")
-
-    # Combine all the elements in the friends lists into a single set
-    all_friends = set()
-    for friends in friends_lists:
-        all_friends = all_friends.union(friends)
-
-    # Calculate the percentage of the server covered by the friends lists
+    all_friends = set(friends_in_f_lists)
     percent_covered = len(all_friends) / server_size
-
     print(f"Percentage of server covered: {percent_covered:.2%}")
 
 if __name__ == "__main__":
